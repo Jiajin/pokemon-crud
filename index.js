eb4e7bcb3a2508210679251553aaaa855be24cf7
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-25 14:59:09 +0800

    left out from prev commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const sequelize = require("./db/index.js");
 
 try {
-  sequelize.authenticate();
+  await sequelize.authenticate();
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
