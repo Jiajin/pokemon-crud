e57ccebf172d73666a7478900e7300dc3f83fedb
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-26 09:40:52 +0800

    add create method w working db sync force

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // index.js
-const sequelize = require("./db/index.js");
+const db = require("./db/models/index.js"); // --> REPLACE THIS
 
-try {
-  await sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+const createPikachu = require("./crud/create"); //  --> ADDED THIS
+// [1] Just test connection, we don't neeed this in actual. --> REMOVE THIS SECTION
+// [2] For dev exploration convenience, we forced synchronisation.
+db.sequelize.sync({ force: true }); // --> REPLACE THIS
+// db.sequelize.sync(); // --> REPLACE THIS
+
+// [3] Use this to refer to the model in the app/router code later
+// const SimplePokemon = await db.SimplePokemon.create();
+
+// [4] Set timeout to simulate async calls to play with model --> ADDED THIS SECTION
+// We need to hold a while for db sync
+setTimeout(createPikachu, 500);
